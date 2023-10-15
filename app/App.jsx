9bc6cdd18a974237e3f271c0cdf03b5cb07882d2
Nihad <nihadishi@gmail.com>
2023-10-15 07:59:24 +0400

    take photo

--- app/App.jsx
+++ app/App.jsx
@@ -1,9 +1,18 @@
-import Routes from "./src/navigation/Routes";
+import {useEffect} from 'react';
+import Routes from './src/navigation/Routes';
+import {Camera} from 'react-native-vision-camera';
 
-function App(){
-  return (
-    <Routes/>
-  );
+function App() {
+  useEffect(() => {
+    checkPermission();
+  }, []);
+  const checkPermission = async () => {
+    const newCameraPermission = await Camera.requestCameraPermission();
+    const newMicrophonePermission = await Camera.requestMicrophonePermission();
+    console.log(newCameraPermission);
+  };
+
+  return <Routes />;
 }
 
 export default App;
